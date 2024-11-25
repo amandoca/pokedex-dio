bbd17daa58d27ac626ed943e579cb120a2b618ec
Amanda Lopes de Souza <amanda.aparecida1905@hotmail.com>
2024-11-24 21:33:45 -0300

    project pokedex dio

--- src/axios/index.js
+++
@@ -1,6 +0,0 @@
-import Factory from './factory';
-
-const coreApiUrl = 'https://pokeapi.co/api/v2';
-const commonInstance = Factory(coreApiUrl);
-
-export default commonInstance;
--- src/store/getters.js
+++
@@ -1,8 +0,0 @@
-import { parsePokemonInfo } from '@/utils';
-import state from './state';
-
-export default {
-	get pokemonsInfo() {
-		return state.list.map(parsePokemonInfo);
-	},
-};
